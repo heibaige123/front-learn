heibaige123/front-learn

commit 576a47bd666d5e5806ef77fd0d021b0afeedc4f1

fix: fix regression on duplicate component init when using shared data objects
fix #7805

packages/multi-packages/vue/vue2/src/core/vdom/create-component.js
@@ -32,7 +32,7 @@ import {
   renderRecyclableComponentTemplate
 } from 'weex/runtime/recycle-list/render-component-template'
 
-// hooks to be invoked on component VNodes during patch
+// inline hooks to be invoked on component VNodes during patch
 const componentVNodeHooks = {
   init (
     vnode: VNodeWithData,
@@ -189,8 +189,8 @@ export function createComponent (
     }
   }
 
-  // merge component management hooks onto the placeholder node
-  mergeHooks(data)
+  // install component management hooks onto the placeholder node
+  installComponentHooks(data)
 
   // return a placeholder vnode
   const name = Ctor.options.name || tag
@@ -234,22 +234,11 @@ export function createComponentInstanceForVnode (
   return new vnode.componentOptions.Ctor(options)
 }
 
-function mergeHooks (data: VNodeData) {
-  if (!data.hook) {
-    data.hook = {}
-  }
+function installComponentHooks (data: VNodeData) {
+  const hooks = data.hook || (data.hook = {})
   for (let i = 0; i < hooksToMerge.length; i++) {
     const key = hooksToMerge[i]
-    const fromParent = data.hook[key]
-    const ours = componentVNodeHooks[key]
-    data.hook[key] = fromParent ? mergeHook(ours, fromParent) : ours
-  }
-}
-
-function mergeHook (one: Function, two: Function): Function {
-  return function (a, b, c, d) {
-    one(a, b, c, d)
-    two(a, b, c, d)
+    hooks[key] = componentVNodeHooks[key]
   }
 }
 

packages/multi-packages/vue/vue2/test/unit/modules/vdom/patch/edge-cases.spec.js
@@ -374,4 +374,36 @@ describe('vdom patch: edge cases', () => {
     expect(vm.$el.querySelector('custom-foo').getAttribute('selected')).toBe('1')
     Vue.config.ignoredElements = []
   })
+
+  // #7805
+  it('should not cause duplicate init when components share data object', () => {
+    const Base = {
+      render (h) {
+        return h('div', this.$options.name)
+      }
+    }
+
+    const Foo = {
+      name: 'Foo',
+      extends: Base
+    }
+
+    const Bar = {
+      name: 'Bar',
+      extends: Base
+    }
+
+    const vm = new Vue({
+      render (h) {
+        const data = { staticClass: 'text-red' }
+
+        return h('div', [
+          h(Foo, data),
+          h(Bar, data)
+        ])
+      }
+    }).$mount()
+
+    expect(vm.$el.textContent).toBe('FooBar')
+  })
 })

packages/multi-packages/vue/vue2/test/unit/modules/vdom/create-component.spec.js
@@ -19,12 +19,10 @@ describe('create-component', () => {
       props: ['msg'],
       render () {}
     }
-    const init = jasmine.createSpy()
     const data = {
       props: { msg: 'hello world' },
       attrs: { id: 1 },
       staticAttrs: { class: 'foo' },
-      hook: { init },
       on: { notify: 'onNotify' }
     }
     const vnode = createComponent(child, data, vm, vm)
@@ -38,9 +36,6 @@ describe('create-component', () => {
     expect(vnode.elm).toBeUndefined()
     expect(vnode.ns).toBeUndefined()
     expect(vnode.context).toEqual(vm)
-
-    vnode.data.hook.init(vnode)
-    expect(init.calls.argsFor(0)[0]).toBe(vnode)
   })
 
   it('create a component when resolved with async loading', done => {

commit d5614f1bb498acb10ce80a8b2087a6ceeeef9e83

fix: ensure init/prepatch hooks are still repsected
this address a regression introduced in 576a47b which
causes vue-router#1338 to resurface.

packages/multi-packages/vue/vue2/src/core/vdom/create-component.js
@@ -238,8 +238,21 @@ function installComponentHooks (data: VNodeData) {
   const hooks = data.hook || (data.hook = {})
   for (let i = 0; i < hooksToMerge.length; i++) {
     const key = hooksToMerge[i]
-    hooks[key] = componentVNodeHooks[key]
+    const existing = hooks[key]
+    const toMerge = componentVNodeHooks[key]
+    if (existing !== toMerge && !(existing && existing._merged)) {
+      hooks[key] = existing ? mergeHook(toMerge, existing) : toMerge
+    }
   }
+}
+
+function mergeHook (f1, f2) {
+  const merged = (a, b, c, d) => {
+    f1(a, b, c, d)
+    f2(a, b, c, d)
+  }
+  merged._merged = true
+  return merged
 }
 
 // transform component v-model info (value and callback) into

packages/multi-packages/vue/vue2/test/unit/modules/vdom/patch/edge-cases.spec.js
@@ -393,9 +393,15 @@ describe('vdom patch: edge cases', () => {
       extends: Base
     }
 
+    // sometimes we do need to tap into these internal hooks (e.g. in vue-router)
+    // so make sure it does work
+    const inlineHookSpy = jasmine.createSpy('inlineInit')
+
     const vm = new Vue({
       render (h) {
-        const data = { staticClass: 'text-red' }
+        const data = { staticClass: 'text-red', hook: {
+          init: inlineHookSpy
+        }}
 
         return h('div', [
           h(Foo, data),
@@ -405,5 +411,6 @@ describe('vdom patch: edge cases', () => {
     }).$mount()
 
     expect(vm.$el.textContent).toBe('FooBar')
+    expect(inlineHookSpy.calls.count()).toBe(2)
   })
 })