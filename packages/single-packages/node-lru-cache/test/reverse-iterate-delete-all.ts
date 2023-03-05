heibaige123/front-learn

commit 6f8978eafd6544e82a13a60713f843c34a889ac1

Allow unbounded storage if maxSize or ttl set
This also prevents setting size=0 if maxSize is set, since that is a
recipe for disaster.

At least one of max, maxSize, or ttl MUST be set, to prevent unbounded
growth of the cache.  And really, without ttlAutopurge, it's effectively
unsafe and unbounded in that case anyway, *especially* if allowStale is
set.  This is potentially "unsafe at any speed" territory, so it emits a
process warning in that case.

If max is not set, then regular Array is used to track items, without
setting an initial Array capacity.  This will often perform much worse,
but in many cases, it's not so bad.  Bigger hazard is definitely
unbounded memory consumption.

Fix: https://github.com/isaacs/node-lru-cache/issues/208

packages/single-packages/node-lru-cache/index.js
@@ -31,7 +31,8 @@ const shouldWarn = code => typeof process === 'object' &&
 
 const warn = (code, what, instead, fn) => {
   warned.add(code)
-  process.emitWarning(`The ${what} is deprecated. Please use ${instead} instead.`, 'DeprecationWarning', code, fn)
+  const msg = `The ${what} is deprecated. Please use ${instead} instead.`
+  process.emitWarning(msg, 'DeprecationWarning', code, fn)
 }
 
 const isPosInt = n => n && n === Math.floor(n) && n > 0 && isFinite(n)
@@ -60,7 +61,7 @@ class ZeroArray extends Array {
 
 class Stack {
   constructor (max) {
-    const UintArray = getUintArray(max)
+    const UintArray = max ? getUintArray(max) : Array
     this.heap = new UintArray(max)
     this.length = 0
   }
@@ -75,7 +76,7 @@ class Stack {
 class LRUCache {
   constructor (options = {}) {
     const {
-      max,
+      max = 0,
       ttl,
       ttlResolution = 1,
       ttlAutopurge,
@@ -85,7 +86,7 @@ class LRUCache {
       disposeAfter,
       noDisposeOnSet,
       noUpdateTTL,
-      maxSize,
+      maxSize = 0,
       sizeCalculation,
     } = options
 
@@ -97,17 +98,17 @@ class LRUCache {
       stale,
     } = options instanceof LRUCache ? {} : options
 
-    if (!isPosInt(max)) {
-      throw new TypeError('max option must be an integer')
+    if (max !== 0 && !isPosInt(max)) {
+      throw new TypeError('max option must be a nonnegative integer')
     }
 
-    const UintArray = getUintArray(max)
+    const UintArray = max ? getUintArray(max) : Array
     if (!UintArray) {
       throw new Error('invalid max value: ' + max)
     }
 
     this.max = max
-    this.maxSize = maxSize || 0
+    this.maxSize = maxSize
     this.sizeCalculation = sizeCalculation || length
     if (this.sizeCalculation) {
       if (!this.maxSize) {
@@ -141,7 +142,7 @@ class LRUCache {
     this.noDisposeOnSet = !!noDisposeOnSet
     this.noUpdateTTL = !!noUpdateTTL
 
-    if (this.maxSize) {
+    if (this.maxSize !== 0) {
       if (!isPosInt(this.maxSize)) {
         throw new TypeError('maxSize must be a positive integer if specified')
       }
@@ -159,6 +160,20 @@ class LRUCache {
         throw new TypeError('ttl must be a positive integer if specified')
       }
       this.initializeTTLTracking()
+    }
+
+    // do not allow completely unbounded caches
+    if (this.max === 0 && this.ttl === 0 && this.maxSize === 0) {
+      throw new TypeError('At least one of max, maxSize, or ttl is required')
+    }
+    if (!this.ttlAutopurge && !this.max && !this.maxSize) {
+      const code = 'LRU_CACHE_UNBOUNDED'
+      if (shouldWarn(code)) {
+        warned.add(code)
+        const msg = 'TTL caching without ttlAutopurge, max, or maxSize can ' +
+          'result in unbounded memory consumption.'
+        process.emitWarning(msg, 'UnboundedCacheWarning', code, LRUCache)
+      }
     }
 
     if (stale) {
@@ -221,9 +236,17 @@ class LRUCache {
     this.calculatedSize = 0
     this.sizes = new ZeroArray(this.max)
     this.removeItemSize = index => this.calculatedSize -= this.sizes[index]
-    this.addItemSize = (index, v, k, size, sizeCalculation) => {
-      const s = size || (sizeCalculation ? sizeCalculation(v, k) : 0)
-      this.sizes[index] = isPosInt(s) ? s : 0
+    this.requireSize = (k, v, size, sizeCalculation) => {
+      if (sizeCalculation && !size) {
+        size = sizeCalculation(v, k)
+      }
+      if (!isPosInt(size)) {
+        throw new TypeError('size must be positive integer')
+      }
+      return size
+    }
+    this.addItemSize = (index, v, k, size) => {
+      this.sizes[index] = size
       const maxSize = this.maxSize - this.sizes[index]
       while (this.calculatedSize > maxSize) {
         this.evict()
@@ -241,7 +264,12 @@ class LRUCache {
     }
   }
   removeItemSize (index) {}
-  addItemSize (index, v, k, size, sizeCalculation) {}
+  addItemSize (index, v, k, size) {}
+  requireSize (k, v, size, sizeCalculation) {
+    if (size || sizeCalculation) {
+      throw new TypeError('cannot set size without setting maxSize on cache')
+    }
+  }
 
   *indexes ({ allowStale = this.allowStale } = {}) {
     if (this.size) {
@@ -389,6 +417,7 @@ class LRUCache {
     sizeCalculation = this.sizeCalculation,
     noUpdateTTL = this.noUpdateTTL,
   } = {}) {
+    size = this.requireSize(k, v, size, sizeCalculation)
     let index = this.size === 0 ? undefined : this.keyMap.get(k)
     if (index === undefined) {
       // addition
@@ -400,7 +429,7 @@ class LRUCache {
       this.prev[index] = this.tail
       this.tail = index
       this.size ++
-      this.addItemSize(index, v, k, size, sizeCalculation)
+      this.addItemSize(index, v, k, size)
       noUpdateTTL = false
     } else {
       // update
@@ -414,7 +443,7 @@ class LRUCache {
         }
         this.removeItemSize(index)
         this.valList[index] = v
-        this.addItemSize(index, v, k, size, sizeCalculation)
+        this.addItemSize(index, v, k, size)
       }
       this.moveToTail(index)
     }

packages/single-packages/node-lru-cache/tap-snapshots/test/deprecations.js.test.cjs
@@ -49,5 +49,11 @@ Array [
     "LRU_CACHE_METHOD_del",
     Function get del(),
   ],
+  Array [
+    "TTL caching without ttlAutopurge, max, or maxSize can result in unbounded memory consumption.",
+    "UnboundedCacheWarning",
+    "LRU_CACHE_UNBOUNDED",
+    Function LRUCache(classLRUCache),
+  ],
 ]
 `

packages/single-packages/node-lru-cache/tap-snapshots/test/map-like.js.test.cjs
@@ -10,35 +10,35 @@ Array [
   Array [
     3,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "3",
     },
   ],
   Array [
     4,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "4",
     },
   ],
   Array [
     5,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "5",
     },
   ],
   Array [
     6,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "6",
     },
   ],
   Array [
     7,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "7",
     },
   ],
@@ -50,7 +50,7 @@ Array [
   Array [
     3,
     Object {
-      "size": 0,
+      "size": 1,
       "ttl": 0,
       "value": "3",
     },
@@ -58,7 +58,7 @@ Array [
   Array [
     5,
     Object {
-      "size": 0,
+      "size": 1,
       "ttl": 0,
       "value": "5",
     },
@@ -66,7 +66,7 @@ Array [
   Array [
     6,
     Object {
-      "size": 0,
+      "size": 1,
       "ttl": 0,
       "value": "6",
     },
@@ -74,7 +74,7 @@ Array [
   Array [
     4,
     Object {
-      "size": 0,
+      "size": 1,
       "ttl": 0,
       "value": "new value 4",
     },
@@ -87,35 +87,35 @@ Array [
   Array [
     3,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "3",
     },
   ],
   Array [
     5,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "5",
     },
   ],
   Array [
     6,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "6",
     },
   ],
   Array [
     7,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "7",
     },
   ],
   Array [
     4,
     Object {
-      "size": 0,
+      "size": 1,
       "value": "new value 4",
     },
   ],

packages/single-packages/node-lru-cache/test/basic.js
@@ -86,6 +86,27 @@ t.test('bad max values', t => {
   t.throws(() => new LRU({ max: 2.5 }))
   t.throws(() => new LRU({ max: Infinity }))
   t.throws(() => new LRU({ max: Number.MAX_SAFE_INTEGER * 2 }))
+
+  // ok to have a max of 0 if maxSize or ttl are set
+  const sizeOnly = new LRU({ maxSize: 100 })
+
+  // setting the size to invalid values
+  t.throws(() => sizeOnly.set('foo', 'bar'), TypeError)
+  t.throws(() => sizeOnly.set('foo', 'bar', { size: 0 }), TypeError)
+  t.throws(() => sizeOnly.set('foo', 'bar', { size: -1 }), TypeError)
+  t.throws(() => sizeOnly.set('foo', 'bar', {
+    sizeCalculation: () => -1,
+  }), TypeError)
+  t.throws(() => sizeOnly.set('foo', 'bar', {
+    sizeCalculation: () => 0,
+  }), TypeError)
+
+  const ttlOnly = new LRU({ ttl: 1000, ttlAutopurge: true })
+  // cannot set size when not tracking size
+  t.throws(() => ttlOnly.set('foo', 'bar', { size: 1 }), TypeError)
+  t.throws(() => ttlOnly.set('foo', 'bar', { size: 1 }), TypeError)
+
+  const sizeTTL = new LRU({ maxSize: 100, ttl: 1000 })
   t.end()
 })
 

packages/single-packages/node-lru-cache/test/deprecations.js
@@ -18,6 +18,9 @@ t.test('warns exactly once for a given deprecation', t => {
   t.equal(c.reset, c.clear)
   t.equal(c.del, c.delete)
 
+  // not technically a "deprecation" but similar
+  new LRU({ ttl: 10 })
+
   t.matchSnapshot(warnings)
 
   warnings.length = 0
@@ -33,6 +36,8 @@ t.test('warns exactly once for a given deprecation', t => {
   t.equal(d.length, 0)
   t.equal(d.prune, d.purgeStale)
   t.equal(d.reset, d.clear)
+  new LRU({ ttl: 10 })
+
   t.strictSame(warnings, [], 'only warn once')
 
   warnings.length = 0

packages/single-packages/node-lru-cache/test/map-like.js
@@ -5,7 +5,7 @@ if (typeof performance === 'undefined') {
 const t = require('tap')
 const LRU = require('../')
 
-const c = new LRU({ max: 5, maxSize: 5 })
+const c = new LRU({ max: 5, maxSize: 5, sizeCalculation: () => 1 })
 
 t.matchSnapshot(c.keys(), 'empty, keys')
 t.matchSnapshot(c.values(), 'empty, values')

packages/single-packages/node-lru-cache/test/size-calculation.js
@@ -42,14 +42,9 @@ t.test('store strings, size = length', t => {
   t.end()
 })
 
-t.test('bad size calculation fn returns 0', t => {
+t.test('bad size calculation fn throws on set()', t => {
   const c = new LRU({ max: 5, maxSize: 5, sizeCalculation: () => 'asdf' })
-  c.set(1, '1'.repeat(100))
-  t.equal(c.size, 1)
-  t.equal(c.calculatedSize, 0)
-  c.set(2, '2'.repeat(100), { sizeCalculation: () => 'foobar' })
-  t.equal(c.size, 2)
-  t.equal(c.calculatedSize, 0)
+  t.throws(() => c.set(1, '1'.repeat(100)), TypeError)
   t.end()
 })
 

packages/single-packages/node-lru-cache/test/ttl.js
@@ -67,7 +67,7 @@ const runTests = (LRU, t) => {
   })
 
   t.test('ttl tests with ttlResolution=100', t => {
-    const c = new LRU({ max: 5, ttl: 10, ttlResolution: 100 })
+    const c = new LRU({ ttl: 10, ttlResolution: 100, max: 10 })
     c.set(1, 1)
     t.equal(c.get(1), 1, '1 get not stale', { now: clock._now })
     clock.advance(5)
@@ -99,7 +99,7 @@ const runTests = (LRU, t) => {
   t.test('ttlResolution only respected if non-negative integer', t => {
     const invalids = [ -1, null, undefined, 'banana', {} ]
     for (const i of invalids) {
-      const c = new LRU({ max: 5, ttlResolution: i })
+      const c = new LRU({ ttl: 5, ttlResolution: i, max: 5 })
       t.not(c.ttlResolution, i)
       t.equal(c.ttlResolution, Math.floor(c.ttlResolution))
       t.ok(c.ttlResolution >= 0)
@@ -108,7 +108,7 @@ const runTests = (LRU, t) => {
   })
 
   t.test('ttlAutopurge', t => {
-    const c = new LRU({ max: 2, ttl: 10, ttlAutopurge: true, ttlResolution: 0 })
+    const c = new LRU({ ttl: 10, ttlAutopurge: true, ttlResolution: 0 })
     c.set(1, 1)
     c.set(2, 2)
     t.equal(c.size, 2)

commit 807efe8cb09b71419b03c0453a9547ebaec29d4e

Fix infinite loop in rindexes with maxless data
Verify that index is not undefined in isValidIndex()

Fixes a bug introduced in 6f8978e, when support for non-preallocated
data bound by ttl and/or maxSize was added.  When the pointer array is a
`number[]` instead of a `UintArray`, unset values will be `undefined`
rather than `0`, resulting in the situation where `index ===
this.keyMap.get(this.keyList[index])` is not a valid test for index
validity, since it can be `undefined` rather than always being
guaranteed to be a number.

Another way to fix this would be ensure that a previously unused spot in
the array is initialized to `0` when pointers are initialized.  However,
in performance critical cases, every set and get matters, and relying on
the initialized state of a UintArray saves that check in several hot
paths.  On the other hand, the test for `index === undefined` only has
to happen during iteration, which is much less likely to be performance
critical.

Fix: https://github.com/isaacs/node-lru-cache/issues/278

packages/single-packages/node-lru-cache/index.js
@@ -488,7 +488,10 @@ class LRUCache {
   }
 
   isValidIndex(index) {
-    return this.keyMap.get(this.keyList[index]) === index
+    return (
+      index !== undefined &&
+      this.keyMap.get(this.keyList[index]) === index
+    )
   }
 
   *entries() {

packages/single-packages/node-lru-cache/test/reverse-iterate-delete-all.ts
@@ -1,0 +1,13 @@
+// https://github.com/isaacs/node-lru-cache/issues/278
+import t from 'tap'
+import LRU from '../'
+const lru = new LRU<string, string>({
+  maxSize:2,
+  sizeCalculation: () => 1,
+});
+lru.set('x', 'x')
+lru.set('y', 'y')
+for (const key of lru.rkeys()) {
+  lru.delete(key)
+}
+t.equal(lru.size, 0)