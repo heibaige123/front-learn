ed0300ed7e783d69094fc4fe3dd4e75d5cd015d7
隔壁的猫先生 <58593878+heibaige123@users.noreply.github.com>
2025-04-04 23:03:51 +0800

    refactor: remove obsolete test files and configurations
    - Deleted reverse-iterate-delete-all.ts, size-calculation.ts, ttl.ts, unbounded-warning.ts, warn-missing-ac.ts test files to clean up the codebase.
    - Removed tsconfig.json and typedoc.json files as they are no longer needed.

--- packages/single-packages/node-lru-cache/packages/multi-packages/vue/node-lru-cache/benchmark/profile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p profiles
-rm -f isolate-*.log profile.txt
-N=1000000 node --prof worker.js
-d=profiles/$(date +%s)
-node --prof-process isolate-*.log > $d
-ln ${PWD}/$d profile.txt
-cat profile.txt
